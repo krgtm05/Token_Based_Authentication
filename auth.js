92f36c062d90da396536f21db5d5a00f1ee1f8d1
krgautamofficial@gmail.com <krgtm05@example.com>
2025-06-26 01:32:35 +0530

    Custom Token Based Auth Completed

--- auth.js
+++ auth.js
@@ -3,7 +3,31 @@ const app = express();
 app.use(express.json()); //middleware used for reading json data from body
 const users = [];
 
-
+//function to generate custom token
+function generateToken() {
+  const options = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
+  let token = "";
+  for (let i = 0; i < 50; i++) {
+    token += options[Math.floor(Math.random() * options.length)];
+  }
+  return token;
+}
 //Sign up route
 app.post("/signup", function (req, res) {
   let username = req.body.username;
@@ -21,17 +45,33 @@ app.post("/signup", function (req, res) {
   });
   res.send("Signed Up Successfully");
 });
-//sign in rout
+//sign in route
 app.post("/signin", function (req, res) {
   let username = req.body.username;
   let password = req.body.password;
-
-  const user = users.find(
-    (u) => u.username === username && u.password === password
-  );
-
-  if (user) res.send("Signed in done");
-  else res.send("Invalid credentials");
+  const token = generateToken();
+  for (let user of users) {
+    if (user.username === username && user.password === password) {
+      user.token = token;
+      res.send(token);
+      return;
+    }
+  }
+  res.send("Invalid Credentials");
+});
+//me route
+app.get("/me", function (req, res) {
+  const token = req.headers["token"];
+  for (let user of users) {
+    if (user.token === token) {
+      res.json({
+        username: user.username,
+        password: user.password,
+      });
+      return;
+    }
+  }
+  res.send("Not found");
 });
 
 app.listen(3000);
